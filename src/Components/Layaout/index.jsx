e26aab49ccd7454773a958ab7c7d788965ddeebd
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-23 10:14:59 -0500

    Update layout and add dynamic card rendering

--- src/Components/Layaout/index.jsx
+++ src/Components/Layaout/index.jsx
@@ -1,4 +1,4 @@
 const Layaout = ({ children }) => {
-  return <div className="flex flex-col items-center mt-14">{children}</div>;
+  return <div className="flex flex-col items-center mt-[72px]">{children}</div>;
 };
 export default Layaout;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
 import Layaout from "../../Components/Layaout";
+import Card from "../../Components/Card";
 
 const Home = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
   return (
     <Layaout>
-      <h1>Home</h1>
+      <div className="w-full px-4 grid gap-4 md:gap-6 grid-cols-[repeat(auto-fit,minmax(224px,1fr))] justify-items-center">
+        {items.map((item) => (
+          <Card key={item.id} />
+        ))}
+      </div>
     </Layaout>
   );
 };
