ce450689765895d25309773402d6d76811588ac5
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-29 11:54:49 -0500

    Add search functionality and filter items by title

--- src/AppContext/index.jsx
+++ src/AppContext/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
@@ -9,10 +9,30 @@ const AppProvider = ({ children }) => {
   const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false);
   const [cartProducts, setCartProducts] = useState([]);
   const [order, setOrder] = useState([]);
+  const [items, setItems] = useState(null);
+  const [filteredItems, setFilteredItems] = useState(null);
+  const [searchByTitle, setSearchByTitle] = useState(null);
   const openProductDetail = () => setIsProductDetailOpen(true);
   const closeProductDetail = () => setIsProductDetailOpen(false);
   const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true);
   const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter((item) =>
+      item.title.toLowerCase().includes(searchByTitle.toLowerCase())
+    );
+  };
+
+  useEffect(() => {
+    if (searchByTitle)
+      setFilteredItems(filteredItemsByTitle(items, searchByTitle));
+  }, [items, searchByTitle]);
 
   return (
     <AppContext.Provider
@@ -31,6 +51,11 @@ const AppProvider = ({ children }) => {
         closeCheckoutSideMenu,
         order,
         setOrder,
+        items,
+        setItems,
+        searchByTitle,
+        setSearchByTitle,
+        filteredItems,
       }}
     >
       {children}
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,16 +1,29 @@
-import { useState, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import Layaout from "../../Components/Layaout";
 import Card from "../../Components/Card";
 import ProductDetail from "../../Components/ProductDetail";
+import { AppContext } from "../../AppContext";
 
 const Home = () => {
-  const [items, setItems] = useState([]);
+  const { items, setSearchByTitle, searchByTitle, filteredItems } =
+    useContext(AppContext);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((data) => setItems(data));
+    setSearchByTitle(null);
   }, []);
+
+  const renderView = () => {
+    if (searchByTitle?.length > 0) {
+      if (filteredItems?.length > 0) {
+        return filteredItems?.map((item) => <Card key={item.id} data={item} />);
+      } else {
+        return <div>{"We don't have anything"}</div>;
+      }
+    } else {
+      return items?.map((item) => <Card key={item.id} data={item} />);
+    }
+  };
+
   return (
     <Layaout>
       <div className="w-full flex flex-col px-4 gap-2 text-gray-color items-center justify-center relative lg:w-80 mb-4">
@@ -19,13 +32,12 @@ const Home = () => {
           type="text"
           placeholder="Search a product"
           className="rounded-lg border bg-card-color text-gray-color border-gray-color w-full p-4 focus:outline-none text-center"
+          onChange={(event) => setSearchByTitle(event.target.value)}
         />
       </div>
       <div className="w-full h-[calc(100vh-70px-90px)] flex items-center justify-center overflow-y-auto mb-6">
-        <div className="w-full h-full max-w-screen-xl grid gap-4 md:gap-6 md:px-9 px-4 grid-cols-[repeat(auto-fit,minmax(224px,1fr))]">
-          {items.map((item) => (
-            <Card key={item.id} data={item} />
-          ))}
+        <div className="w-full h-full max-w-screen-xl grid gap-4 md:gap-6 md:px-9 px-4 lg:grid-cols-4 md:grid-cols-3 grid-rows-[repeat(auto-fit,125px)] md:grid-rows-[repeat(auto-fit,244px)] ">
+          {renderView()}
         </div>
       </div>
       <ProductDetail />
